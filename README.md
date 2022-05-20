fa455b7787b45067abab330ad889788be5db1bef
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-05-20 10:27:49 -0500

    Add image

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# twillio-hatch-apprenticeship
+![KGSjze3YOpK_plTq6_DHfy3nvx3klI2qZn9l1GVPFwFO9G width-500](https://user-images.githubusercontent.com/55994508/169561264-57f69e67-355c-42e0-b03d-90beaee107d6.png)
---
+++ script.js
@@ -0,0 +1,43 @@
+// // Test
+// // 1. FizzBuzz
+
+// function fizzBuzz(n) {
+//     // Write your code here
+//     for (var i = 1; i <= n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('FizzBuzz');
+//         } else if (i % 3 === 0) {
+//             console.log('Fizz');
+//         } else if (i % 5 === 0) {
+//             console.log('Buzz');
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// ** Twilio **
+
+// 2. Single Image Size Validation
+
+// function validateImageSize(imageUrls, maxSize) {
+// Write your code here
+// }
+
+// console.log([['http://www.image.com/image.png', '32000000']], '20MB');
+
+//3. What's in a number?
+
+// function validatePhoneNumberFormat(address) {
+// Write your code here
+// }
+
+// console.log(validatePhoneNumberFormat(34864683619)); //Whatsapp
+// console.log(validatePhoneNumberFormat(+12025550001)); // messenger
+// console.log(validatePhoneNumberFormat(gh_a1f37dacefe3)); //wechat
+
+//Invalid output: "INVALID_ADDRESS"
+// For valid E.164 format output: "SMS"
+// for Whatsapp output: "WHATSAPP"
+// for Wechat output: "WECHAT"
+// for Messenger output: "MESSENGER"
